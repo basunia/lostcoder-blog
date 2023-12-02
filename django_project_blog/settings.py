4cfa4f4409998fe4ef19c3cafe0cadef89020bc6
Mahmud Basunia <mahmud.basunia@gmail.com>
2023-12-02 21:59:13 +0600

    Minor tweaks

--- django_project_blog/settings.py
+++ django_project_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-a+^+kt&4#27#r^2dawo%#73o76r0m_wma^@ahdsf7zr9=7#vwu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lostcoder.net']
+ALLOWED_HOSTS = ['lostcoder.net','127.0.0.1']
 
 
 # Application definition
@@ -124,3 +125,16 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     'blog/static',
+# ]
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
