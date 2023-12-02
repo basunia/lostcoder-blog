c768f590d3d10649a44de3524e0c745b40ff4886
Mahmud Basunia <mahmud.basunia@gmail.com>
2023-12-02 09:21:23 +0600

    Add person class method

--- blog/persion.py
+++ blog/persion.py
@@ -1,16 +1,18 @@
 class Persion:
-    name = ''
-    age = 0
+    # name = ""
+    # age = 0
+
     def __init__(self, persion_name, person_age) -> None:
         self.name = persion_name
         self.age = person_age
-        
-    def __str__(self) -> str:
-        return f'Mahmud class'
-    
-    def showPersion(self) -> str:
-        return f'name {self.name} age {self.age}'
 
-p = Persion(persion_name='Mahmud', person_age=43)
+    def __str__(self) -> str:
+        return f"Mahmud class"
+
+    def show_persion(self) -> str:
+        return f"name {self.name} age {self.age}"
+
+
+p = Persion(persion_name="Mahmud", person_age=43)
 print(p)
-print(p.showPersion())
+print(p.show_persion())
