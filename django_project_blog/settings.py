e8de2987265652b8b854808611789ab65ad2a314
Mahmud Basunia <mahmud.basunia@gmail.com>
2023-12-03 18:21:56 -0800

    update static_root to public_html directory

--- django_project_blog/settings.py
+++ django_project_blog/settings.py
@@ -131,10 +131,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 #     'blog/static',
 # ]
 STATIC_DIR = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+# STATICFILES_DIRS = [
+#     STATIC_DIR,
+# ]
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
+STATIC_ROOT = "/home/lostcode/public_html/static/"
