99f2b25c4c391c34b1c57de811d214c786366dc0
Mahmud Basunia <mahmud.basunia@gmail.com>
2024-07-09 16:33:08 +0600

    Create ingredients app

--- django_project_blog/settings.py
+++ django_project_blog/settings.py
@@ -39,11 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "livereload",
+    # "livereload",
     "django.contrib.staticfiles",
-    "example_app",
     "blog",
     "users",
+    "ingredients"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "livereload.middleware.LiveReloadScript"
+    # "livereload.middleware.LiveReloadScript"
 ]
 
 ROOT_URLCONF = "django_project_blog.urls"
